0cbdccf3c5a53e09a9ade44d52edc5db1cdad0ad
Matthew Todd <matthew@matthewtodd.org>
2024-09-05 09:10:45 -0400

    DMARC

--- dns/valerietodd.org.tf
+++ dns/valerietodd.org.tf
@@ -61,6 +61,14 @@ resource "aws_route53_record" "txt" {
   records = ["v=spf1 include:_spf.google.com ~all"]
 }
 
+resource "aws_route53_record" "dmarc" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "_dmarc.valerietodd.org"
+  type    = "TXT"
+  ttl     = 3600
+  records = ["v=DMARC1; p=reject"]
+}
+
 resource "aws_route53_record" "github_pages_challenge" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "_github-pages-challenge-matthewtodd.valerietodd.org"
